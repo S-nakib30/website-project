b1755113af24b6e24558f435b7aab82762cdef9f
S-nakib30 <s.nakib30@gmail.com>
2025-07-01 18:28:05 +0600

    final

--- Tutor_website-project/script.js
+++ Tutor_website-project/script.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.querySelector('.mobile-menu-toggle');
-  const mobileMenu = document.querySelector('.mobile_menu_item');
+  const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
+  const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
 
   toggleButton.addEventListener('click', function () {
-    mobileMenu.classList.toggle('active'); 
+    mobileMenu.classList.toggle('active');
+  });
 });
 
-WebTransportBidirectionalStream.addEventListener('scroll', function(){
-const navbar = document.querySelector('.navbar');
-if(window.screenY>0){
+// Change navbar background on scroll
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+
+  if (window.scrollY > 0) {
     navbar.classList.add('navbar-scroll');
-}else{
-    navbar.classList.remove('navbar-scroll')
-}
-})
+  } else {
+    navbar.classList.remove('navbar-scroll');
+  }
+});
+
+
